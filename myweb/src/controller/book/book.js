00626dc10be582765f9f014009d2606765c66742
16671300229 <16671300229@163.com>
2021-04-26 13:39:32 +0800

    v2.0

--- myweb/src/controller/book/book.js
+++ myweb/src/controller/book/book.js
@@ -29,4 +29,11 @@ module .exports=class extends Index{
         }
     }
 
+    async downloadAction(){
+        const filePath=this.get("path");
+        const path=filePath.replace("http://127.0.0.1:8201",think.ROOT_PATH+"/www");
+        const fileName=filePath.replace("http://127.0.0.1:8201/static/image/","");
+        this.download(path,fileName);
+    }
+
 }
